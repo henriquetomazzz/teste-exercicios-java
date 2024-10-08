52809e6ce2630d570c23e03ef533392579d31d52
henriquetomazzz <henriquetomaz021@gmail.com>
2024-10-08 15:20:40 -0300

    feat: adiciona enunciado do exercicio02

---
+++ poo/exercicio02/Calculadora.java
@@ -0,0 +1,3 @@
+/*Crie uma classe Calculadora com métodos para realizar operações matemáticas simples (soma, subtração, multiplicação
+e divisão). Não é necessário criar atributos internos. O objetivo é observar como você implementará os métodos. Crie
+objetos Calculadora e execute operações com eles.*/
